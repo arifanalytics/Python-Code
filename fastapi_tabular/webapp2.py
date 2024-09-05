c495263a683f5f203c1b7b3f9b982a6d22fac4c7
MagicDash91 <75797622+MagicDash91@users.noreply.github.com>
2024-09-05 17:33:13 +0700

    Add files via upload

---
+++ fastapi_tabular/webapp2.py
@@ -0,0 +1,464 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('Agg')
+import numpy as np
+import google.generativeai as genai
+from PIL import Image
+from werkzeug.utils import secure_filename
+import os
+import json
+from fpdf import FPDF
+from fastapi import FastAPI, File, UploadFile, Form, HTTPException
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from starlette.requests import Request
+import shutil
+import re
+from pydantic import BaseModel
+from typing import List
+from IPython.display import display, Markdown
+import textwrap
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+sns.set_theme(color_codes=True)
+uploaded_df = None
+
+# Define Pydantic models for requests and responses
+class AnalyzeDocumentRequest(BaseModel):
+    api_key: str
+    custom_question: str
+
+class AnalyzeDocumentResponse(BaseModel):
+    meta: dict
+    plot1_path: str
+    response1: str
+    plot2_path: str
+    response2: str
+    pdf_file_path: str
+
+class MulticlassRequest(BaseModel):
+    api_key: str
+    custom_question: str
+    target_variable: str
+    columns_for_analysis: str  # Expecting comma-separated string
+
+class MulticlassResponse(BaseModel):
+    meta: dict
+    plot3_path: str
+    response3: str
+    plot4_path: str
+    response4: str
+    pdf_file_path: str
+
+def format_text(text):
+    # Replace **text** with <b>text</b>
+    text = re.sub(r'\*\*(.*?)\*\*', r'<b>\1</b>', text)
+    # Replace any remaining * with <br>
+    text = text.replace('*', '<br>')
+    return text
+
+def clean_data(df):
+    # Step 1: Clean currency-related columns
+    for col in df.columns:
+        if any(x in col.lower() for x in ['value', 'price', 'cost', 'amount']):
+            if df[col].dtype == 'object':
+                df[col] = df[col].str.replace('$', '').str.replace('£', '').str.replace('€', '').replace('[^\d.-]', '', regex=True).astype(float)
+    
+    # Step 2: Drop columns with more than 25% missing values
+    null_percentage = df.isnull().sum() / len(df)
+    columns_to_drop = null_percentage[null_percentage > 0.25].index
+    df.drop(columns=columns_to_drop, inplace=True)
+    
+    # Step 3: Fill missing values for remaining columns
+    for col in df.columns:
+        if df[col].isnull().sum() > 0:
+            if null_percentage[col] <= 0.25:
+                if df[col].dtype in ['float64', 'int64']:
+                    median_value = df[col].median()
+                    df[col].fillna(median_value, inplace=True)
+    
+    # Step 4: Convert object-type columns to lowercase
+    for col in df.columns:
+        if df[col].dtype == 'object':
+            df[col] = df[col].str.lower()
+
+    # Step 5: Drop columns with only one unique value
+    unique_value_columns = [col for col in df.columns if df[col].nunique() == 1]
+    df.drop(columns=unique_value_columns, inplace=True)
+
+    return df
+
+
+
+
+def clean_data2(df):
+    for col in df.columns:
+        if 'value' in col or 'price' in col or 'cost' in col or 'amount' in col or 'Value' in col or 'Price' in col or 'Cost' in col or 'Amount' in col:
+            if df[col].dtype == 'object':
+                df[col] = df[col].str.replace('$', '')
+                df[col] = df[col].str.replace('£', '')
+                df[col] = df[col].str.replace('€', '')
+                df[col] = df[col].replace('[^\d.-]', '', regex=True).astype(float)
+
+
+    for col in df.columns:
+        if df[col].dtype == 'object':
+            df[col] = df[col].str.lower()
+
+    return df
+
+
+
+def generate_plot(df, plot_path, plot_type):
+    df = clean_data(df)
+    excluded_words = ["name", "postal", "date", "phone", "address", "code", "id"]
+
+    if plot_type == 'countplot':
+        cat_vars = [col for col in df.select_dtypes(include='object').columns
+                    if all(word not in col.lower() for word in excluded_words) and df[col].nunique() > 1]
+
+        for col in cat_vars:
+            if df[col].nunique() > 10:
+                top_categories = df[col].value_counts().index[:10]
+                df[col] = df[col].apply(lambda x: x if x in top_categories else 'Other')
+
+        num_cols = len(cat_vars)
+        num_rows = (num_cols + 1) // 2
+        fig, axs = plt.subplots(nrows=num_rows, ncols=2, figsize=(15, 5 * num_rows))
+        axs = axs.flatten()
+
+        for i, var in enumerate(cat_vars):
+            category_counts = df[var].value_counts()
+            top_values = category_counts.index[:10][::-1]
+            filtered_df = df.copy()
+            filtered_df[var] = pd.Categorical(filtered_df[var], categories=top_values, ordered=True)
+            sns.countplot(x=var, data=filtered_df, order=top_values, ax=axs[i])
+            axs[i].set_title(var)
+            axs[i].tick_params(axis='x', rotation=30)
+
+            total = len(filtered_df[var])
+            for p in axs[i].patches:
+                height = p.get_height()
+                axs[i].annotate(f'{height / total:.1%}', (p.get_x() + p.get_width() / 2., height), ha='center', va='bottom')
+
+            sample_size = filtered_df.shape[0]
+            axs[i].annotate(f'Sample Size = {sample_size}', xy=(0.5, 0.9), xycoords='axes fraction', ha='center', va='center')
+
+        for i in range(num_cols, len(axs)):
+            fig.delaxes(axs[i])
+
+    elif plot_type == 'histplot':
+        num_vars = [col for col in df.select_dtypes(include=['int', 'float']).columns
+                    if all(word not in col.lower() for word in excluded_words)]
+        num_cols = len(num_vars)
+        num_rows = (num_cols + 2) // 3
+        fig, axs = plt.subplots(nrows=num_rows, ncols=min(3, num_cols), figsize=(15, 5 * num_rows))
+        axs = axs.flatten()
+
+        plot_index = 0
+
+        for i, var in enumerate(num_vars):
+            if len(df[var].unique()) == len(df):
+                fig.delaxes(axs[plot_index])
+            else:
+                sns.histplot(df[var], ax=axs[plot_index], kde=True, stat="percent")
+                axs[plot_index].set_title(var)
+                axs[plot_index].set_xlabel('')
+
+            sample_size = df.shape[0]
+            axs[i].annotate(f'Sample Size = {sample_size}', xy=(0.5, 0.9), xycoords='axes fraction', ha='center', va='center')
+
+            plot_index += 1
+
+        for i in range(plot_index, len(axs)):
+            fig.delaxes(axs[i])
+
+    fig.tight_layout()
+    fig.savefig(plot_path)
+    plt.close(fig)
+    return plot_path
+
+
+@app.post("/result", response_model=AnalyzeDocumentResponse)
+async def result(api_key: str = Form(...), 
+                 file: UploadFile = File(...), 
+                 custom_question: str = Form(...)):
+    global uploaded_df
+
+    if file.filename == '':
+        raise HTTPException(status_code=400, detail="No file selected")
+
+    uploaded_filename = secure_filename(file.filename)
+    file_path = os.path.join("static", uploaded_filename)
+
+    with open(file_path, 'wb') as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    if uploaded_filename.endswith('.csv'):
+        df = pd.read_csv(file_path)
+    elif uploaded_filename.endswith('.xlsx'):
+        df = pd.read_excel(file_path)
+    else:
+        raise HTTPException(status_code=400, detail="Unsupported file format")
+
+    def generate_gemini_response(plot_path):
+        genai.configure(api_key=api_key)
+        model = genai.GenerativeModel('gemini-1.5-flash-latest')
+        img = Image.open(plot_path)
+        response = model.generate_content([custom_question + " Analyze the data insights from the chart.", img])
+        response.resolve()
+        return response.text
+
+    try:
+        plot1_path = generate_plot(df, 'static/plot1.png', 'countplot')
+        plot2_path = generate_plot(df, 'static/plot2.png', 'histplot')
+
+        response1 = generate_gemini_response(plot1_path)
+        response2 = generate_gemini_response(plot2_path)
+
+        uploaded_df = df
+
+        outputs = {
+            "barchart_visualization": plot1_path,
+            "gemini_response1": response1,
+            "histoplot_visualization": plot2_path,
+            "gemini_response2": response2
+        }
+
+        with open("output.json", "w") as outfile:
+            json.dump(outputs, outfile)
+
+        pdf = FPDF()
+        pdf.set_font("Arial", size=12)
+
+        pdf.add_page()
+        pdf.cell(200, 10, txt="Single Countplot Barchart", ln=True, align='C')
+        pdf.image(plot1_path, x=10, y=30, w=190)
+        pdf.add_page()
+        pdf.cell(200, 10, txt="Single Countplot Barchart Google Gemini Response", ln=True, align='C')
+        pdf.ln(10)
+        pdf.multi_cell(0, 10, response1)
+
+        pdf.add_page()
+        pdf.cell(200, 10, txt="Single Histoplot", ln=True, align='C')
+        pdf.image(plot2_path, x=10, y=30, w=190)
+        pdf.add_page()
+        pdf.cell(200, 10, txt="Single Histoplot Google Gemini Response", ln=True, align='C')
+        pdf.ln(10)
+        pdf.multi_cell(0, 10, response2)
+
+        pdf_file_path = os.path.join("static", "output.pdf")
+        pdf.output(pdf_file_path)
+
+        pdf_file_path = os.path.join("static", "output.pdf")
+        pdf_file_path = pdf_file_path.replace("\\", "/")
+
+        return AnalyzeDocumentResponse(
+            meta={"status": "success", "code": 200},
+            plot1_path=plot1_path,
+            response1=response1,
+            plot2_path=plot2_path,
+            response2=response2,
+            pdf_file_path=pdf_file_path
+        )
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+
+@app.post("/multiclass", response_model=MulticlassResponse)
+async def multiclass(
+    request: Request,
+    target_variable: str = Form(...),
+    custom_question: str = Form(...),
+    api_key: str = Form(...),
+    file: UploadFile = File(...),
+    columns_for_analysis: str = Form(...),  # Changed to str to handle CSV string input
+):
+    try:
+        # Read the file content into a DataFrame
+        if file.filename.endswith('.csv'):
+            df = pd.read_csv(file.file, encoding='utf-8')
+        elif file.filename.endswith('.xlsx'):
+            df = pd.read_excel(file.file)
+        else:
+            raise HTTPException(status_code=400, detail="Unsupported file format")
+
+        # Process the columns_for_analysis
+        columns_for_analysis_list = [col.strip() for col in columns_for_analysis.split(',')]
+
+        # Ensure the columns exist in the DataFrame
+        missing_cols = [col for col in columns_for_analysis_list if col not in df.columns]
+        if missing_cols:
+            raise HTTPException(status_code=400, detail=f"Columns not found in the dataset: {', '.join(missing_cols)}")
+
+        # Select the target variable and columns for analysis from the DataFrame
+        if target_variable not in df.columns:
+            raise HTTPException(status_code=400, detail=f"Target variable '{target_variable}' not found in the dataset")
+
+        target_variable_data = df[target_variable]
+        columns_for_analysis_data = df[columns_for_analysis_list]
+
+        # Concatenate target variable and columns for analysis into a single DataFrame
+        df = pd.concat([target_variable_data, columns_for_analysis_data], axis=1)
+
+        # Clean the data (if needed)
+        df = clean_data2(df)
+
+        # Generate visualizations
+
+        # Multiclass Barplot
+        excluded_words = ["name", "postal", "date", "phone", "address", "id"]
+
+        # Get the names of all columns with data type 'object' (categorical variables)
+        cat_vars = [col for col in df.select_dtypes(include=['object']).columns
+                    if all(word not in col.lower() for word in excluded_words)]
+
+        # Exclude the target variable from the list if it exists in cat_vars
+        if target_variable in cat_vars:
+            cat_vars.remove(target_variable)
+
+        # Create a figure with subplots, but only include the required number of subplots
+        num_cols = len(cat_vars)
+        num_rows = (num_cols + 2) // 3  # To make sure there are enough rows for the subplots
+        fig, axs = plt.subplots(nrows=num_rows, ncols=3, figsize=(15, 5*num_rows))
+        axs = axs.flatten()
+
+        # Create a count plot for each categorical variable
+        for i, var in enumerate(cat_vars):
+            top_categories = df[var].value_counts().nlargest(5).index
+            filtered_df = df[df[var].notnull() & df[var].isin(top_categories)]  # Exclude rows with NaN values in the variable
+
+            # Replace less frequent categories with "Other" if there are more than 5 unique values
+            if df[var].nunique() > 5:
+                other_categories = df[var].value_counts().index[5:]
+                filtered_df[var] = filtered_df[var].apply(lambda x: x if x in top_categories else 'Other')
+
+            sns.countplot(x=var, hue=target_variable, data=filtered_df, ax=axs[i], stat="percent")
+            axs[i].set_xticklabels(axs[i].get_xticklabels(), rotation=45)
+
+            # Change y-axis label to represent percentage
+            axs[i].set_ylabel('Percentage')
+
+            # Annotate the subplot with sample size
+            sample_size = df.shape[0]
+            axs[i].annotate(f'Sample Size = {sample_size}', xy=(0.5, 0.9), xycoords='axes fraction', ha='center', va='center')
+
+        # Remove any remaining blank subplots
+        for i in range(num_cols, len(axs)):
+            fig.delaxes(axs[i])
+
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+        plot3_path = "static/multiclass_barplot.png"
+        plt.savefig(plot3_path)
+        plt.close(fig)
+
+        # Multiclass Histplot
+        int_vars = df.select_dtypes(include=['int', 'float']).columns.tolist()
+        int_vars = [col for col in int_vars if col != target_variable]
+
+        # Create a figure with subplots
+        num_cols = len(int_vars)
+        num_rows = (num_cols + 2) // 3  # To make sure there are enough rows for the subplots
+        fig, axs = plt.subplots(nrows=num_rows, ncols=3, figsize=(15, 5*num_rows))
+        axs = axs.flatten()
+
+        # Create a histogram for each integer variable with hue=target_variable
+        for i, var in enumerate(int_vars):
+            sns.histplot(data=df, x=var, hue=target_variable, kde=True, ax=axs[i], stat="percent")
+            axs[i].set_title(var)
+
+            # Annotate the subplot with sample size
+            sample_size = df.shape[0]
+            axs[i].annotate(f'Sample Size = {sample_size}', xy=(0.5, 0.9), xycoords='axes fraction', ha='center', va='center')
+
+        # Remove any extra empty subplots if needed
+        if num_cols < len(axs):
+            for i in range(num_cols, len(axs)):
+                fig.delaxes(axs[i])
+
+        fig.tight_layout()
+        plt.xticks(rotation=45)
+        plot4_path = "static/multiclass_histplot.png"
+        plt.savefig(plot4_path)
+        plt.close(fig)
+
+        # Google Gemini Responses
+        genai.configure(api_key=api_key)
+
+        # Response for the barplot
+        img_barplot = Image.open(plot3_path)
+        model = genai.GenerativeModel('gemini-1.5-flash-latest')
+        response3 = model.generate_content([custom_question, img_barplot]).text
+
+        # Response for the histplot
+        img_histplot = Image.open(plot4_path)
+        response4 = model.generate_content([custom_question, img_histplot]).text
+
+        # Create a dictionary to store the outputs
+        outputs = {
+            "multiBarchart_visualization": plot3_path,
+            "gemini_response3": response3,
+            "multiHistoplot_visualization": plot4_path,
+            "gemini_response4": response4
+        }
+
+        # Save the dictionary as a JSON file
+        with open("output1.json", "w") as outfile:
+            json.dump(outputs, outfile)
+
+        def safe_encode(text):
+            try:
+                return text.encode('latin1', errors='replace').decode('latin1')  # Replace invalid characters
+            except Exception as e:
+                return f"Error encoding text: {str(e)}"
+
+        # Generate PDF with the results
+        pdf = FPDF()
+        pdf.set_font("Arial", size=12)
+
+        # Add content to the PDF
+        # Multiclass Countplot Barchart and response
+        pdf.add_page()
+        pdf.cell(200, 10, txt="Multiclass Countplot Barchart", ln=True, align='C')
+        pdf.image(plot3_path, x=10, y=30, w=190)
+        pdf.add_page()
+        pdf.cell(200, 10, txt="Multiclass Countplot Barchart Google Gemini Response", ln=True, align='C')
+        pdf.ln(10)
+        pdf.multi_cell(0, 10, safe_encode(response3))
+
+        # Multiclass Histplot and response
+        pdf.add_page()
+        pdf.cell(200, 10, txt="Multiclass Histplot", ln=True, align='C')
+        pdf.image(plot4_path, x=10, y=30, w=190)
+        pdf.add_page()
+        pdf.cell(200, 10, txt="Multiclass Histplot Google Gemini Response", ln=True, align='C')
+        pdf.ln(10)
+        pdf.multi_cell(0, 10, safe_encode(response4))
+
+        pdf_output_path = 'static/analysis_report_complete.pdf'
+        pdf.output(pdf_output_path)
+        pdf_file_path = pdf_output_path.replace("\\", "/")
+
+        return MulticlassResponse(
+            meta={"status": "success", "code": 200},
+            plot3_path=plot3_path,
+            plot4_path=plot4_path,
+            response3=response3,
+            response4=response4,
+            pdf_file_path=pdf_file_path
+        )
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
